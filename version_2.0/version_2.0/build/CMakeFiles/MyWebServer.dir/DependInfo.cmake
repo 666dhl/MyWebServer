c43e37ab11468409df235c86c817895c6c6c1476
Hualun Dong <96689637+666dhl@users.noreply.github.com>
2022-02-09 21:09:13 +0800

    Add files via upload

---
+++ version_2.0/version_2.0/build/CMakeFiles/MyWebServer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/include/epoll.cpp" "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/build/CMakeFiles/MyWebServer.dir/include/epoll.cpp.o"
+  "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/include/requestData.cpp" "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/build/CMakeFiles/MyWebServer.dir/include/requestData.cpp.o"
+  "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/include/threadpool.cpp" "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/build/CMakeFiles/MyWebServer.dir/include/threadpool.cpp.o"
+  "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/include/util.cpp" "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/build/CMakeFiles/MyWebServer.dir/include/util.cpp.o"
+  "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/src/main.cpp" "/home/jiujielun/Desktop/Myserver/MyWebServer-master-test/version_2.0/build/CMakeFiles/MyWebServer.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ version_2.0/version_2.0/build/CMakeFiles/MyWebServer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyWebServer.dir/src/main.cpp.o"
+  "CMakeFiles/MyWebServer.dir/include/requestData.cpp.o"
+  "CMakeFiles/MyWebServer.dir/include/util.cpp.o"
+  "CMakeFiles/MyWebServer.dir/include/epoll.cpp.o"
+  "CMakeFiles/MyWebServer.dir/include/threadpool.cpp.o"
+  "../bin/MyWebServer.pdb"
+  "../bin/MyWebServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyWebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
